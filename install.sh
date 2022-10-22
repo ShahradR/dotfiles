89d835b7d25849d8b4fecb8b0ff4cf5f6d2046b3
Shahrad Rezaei <shahrad@rezaei.io>
2022-10-22 23:49:44 +0000

    feat: add icons to file listings with logo-ls
    Configure Yash-Handa/logo-ls, a "modern ls command with vscode like file icon
    and Git integrations". An alias to the `logo-ls -D` command has also been added
    in the ~/.bash_aliases file, allowing the command to be invoked by running ls.
    
    For more information on the logo-ls project, see https://bit.ly/3F7IAg7

--- install.sh
+++ install.sh
@@ -1,3 +1,14 @@
-#! /bin/sh
+#! /bin/bash -x
 
 cp -a ./dotfiles/. ~/
+
+# Install https://github.com/Yash-Handa/logo-ls
+logols_dir=$(mktemp -d -t logo-ls-XXXXXX)
+
+pushd $logols_dir
+    wget https://github.com/Yash-Handa/logo-ls/releases/download/v1.3.7/logo-ls_amd64.deb
+    sudo dpkg -i ./logo-ls_amd64.deb
+popd
+
+# Alias logo-ls to ls
+printf "alias ls='logo-ls -D'\n" >> ~/.bash_aliases
